572f20c4291c186555e3f99171cbc3ee3e5ae3bf
Sharang Wakankar <sharang.wakankar@hotmail.com>
2020-02-18 19:15:03 -0600

    Add dropbox link for model

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 import urllib.request
 from fastai.vision import *
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse, PlainTextResponse
 from flask import Blueprint, render_template, request, Flask
 
-# app = Starlette()
+export_file_url = 'https://www.dropbox.com/s/v5unkltx0id5k1z/export.pkl?dl=1'
+export_file_name = 'export.pkl'
 
 def create_app():
     application = Flask(__name__)
@@ -15,31 +14,12 @@ app = create_app()
 ###################
 
 classes = ['apples', 'oranges']
-path = Path('.')
+# path = Path('.')
+path = Path(__file__).parent
 fruitsPath = Path('data')
 
-# for c in classes:
-#     print(c)
-#     verify_images(path/c, delete=True, max_size=500)
-#
-#
-# np.random.seed(42)
-# data = ImageDataBunch.from_folder(path, train=".", valid_pct=0.2,
-#                                   ds_tfms=get_transforms(), size=224, num_workers=4).normalize(imagenet_stats)
-#
-# print(data.classes, data.c, len(data.train_ds), len(data.valid_ds))
-
-# train model
-# fruitLearner = cnn_learner(data, models.resnet34, metrics=error_rate)
-# fruitLearner.save('model1')
-
-# classifier
-# data2 = ImageDataBunch.create_from_ll(path, classes, tfms=get_transforms(), size=224).normalize(imagenet_stats)
-# learn = create_cnn(data2, models.resnet34)
-# learn.load('model1')
-
 # model
-fruitLearner = load_learner(path, 'export.pkl')
+fruitLearner = load_learner(path, export_file_name)
 
 def classify(bytes):
     img = open_image(BytesIO(bytes))
@@ -47,19 +27,11 @@ def classify(bytes):
     # print("fruit is: ", pred_class)
     return pred_class
 
-# predict
-#img = open_image(fruitsPath/'oranges'/'00000000.jpg')
-# fruit = classify(img)
-# print("FRUIT: ",fruit)
-
-# pred_class, pred_idx, outputs = fruitLearner.predict(img)
-# print("fruit is: ", pred_class)
-
 ###########################
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    defaults.device = torch.device('cpu')
+    # defaults.device = torch.device('cpu')
 
     if request.method == "POST":
         print("before getting image")
